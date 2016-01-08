1693691f194e1eb5ee5a32de766a8cc5d4bedadd
Will Cauthen <willcauthen50@gmail.com>
2016-01-08 14:37:55 -0800

    navbar is functioning

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
 
-
+# get sprockets
+gem 'sprockets-rails', :require => 'sprockets/railtie'
+# get skeleton
+gem 'skeleton-rails', '~> 0.1.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
 # Use sqlite3 as the database for Active Record
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Divebar
   class Application < Rails::Application
+    # Asset Pipeline
+    config.assets.enabled = true
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
