b4e516aa621fe039e6b2e9c1eacf1480972ef7d0
Will Cauthen <willcauthen50@gmail.com>
2016-01-08 16:51:40 -0800

    policies refactored and semi-legible

---
+++ app/controllers/map_controller.rb
@@ -0,0 +1,5 @@
+class MapController < ApplicationController
+	def index
+		render :index
+	end
+end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+	def index
+		render :splash
+	end
+end
